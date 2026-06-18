6e99a52aa2ec1489cbe71ee4d10f36078c95f501
ZuzuLletto <ybingol3463@gmail.com>
2026-06-18 10:39:06 +0300

    output export olarak guncellendi

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // 'standalone' yerine 'export' yazmalısın.
   images: {
-    unoptimized: true, // Statik export'ta image optimization hatası almamak için bu şart.
+    unoptimized: true,
   },
 }
 
